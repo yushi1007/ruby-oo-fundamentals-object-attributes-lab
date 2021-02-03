1aa372d151685f1835e0c7afa80c20d7bb6d5080
Yu Shi <yushiys95@gmail.com>
2021-02-02 23:01:02 -0500

    commit tree 782fedb4d09f6263fe29d377c8a833d7c0b3972d

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,18 @@
-#Hello Moon
+class Person
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job 
+    end
+end
